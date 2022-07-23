4a04351f33ba336cd6a95dd4d33c7ddd2bdc255e
DamodaraBarbosa <damodarabarbosa@gmail.com>
2022-07-23 19:17:20 -0300

    Geração procedural de artistas e bandas

---
+++ procedural_art_banda.py
@@ -0,0 +1,10 @@
+from random import randint, uniform, choice, randrange
+from func_art_banda import nome_art_banda
+
+genero = ['MPB', 'Rock', 'Indie', 'Funk', 'Sertanejo', 'Hip hop', 'Samba', 'Pop', 'Eletrônica']
+
+art_banda = dict()
+catalogo = list()
+
+print(nome_art_banda(randint(1, 100), 'Funk'))
+
